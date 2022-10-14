94408bed60d7cfce1d46bc20ea94512f0457be1c
Nguyen Duy Quoc <nguyenduyquoc129829042001@gmail.com>
2022-10-14 13:44:15 +0700

    cm

--- src/assignment2/Main.java
+++ src/assignment2/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args){
 
         Account ac1 = new Account(1 , "Giang", 100000);
-//        ac1.credit(200000);
-//        ac1.debit(50000);
-//        ac1.debit(-20000);
+        ac1.credit(200000);
+        ac1.debit(50000);
+        ac1.debit(-20000);
 
         Account ac2 = new Account(2, "Quoc", 50000);
 
